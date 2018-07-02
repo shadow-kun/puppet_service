9817d7dd674cf464b656a79c2b3be4496e31e1b7
shadow-kun <shadowreaper@shadowreaper.net>
2018-07-03 09:43:14 +1000

    [Add] Puppet_Service
    Puppet Service

---
+++ manifests/init.pp
@@ -0,0 +1,53 @@
+# Class: puppet_service
+# ===========================
+#
+# A class that ensures that puppet is enabled and running on all devices.
+#
+# Parameters
+# ----------
+#
+# Document parameters here.
+#
+# * `sample parameter`
+# Explanation of what this parameter affects and what it defaults to.
+# e.g. "Specify one or more upstream ntp servers as an array."
+#
+# Variables
+# ----------
+#
+# Here you should define a list of variables that this module would require.
+#
+# * `sample variable`
+#  Explanation of how this variable affects the function of this class and if
+#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#  External Node Classifier as a comma separated list of hostnames." (Note,
+#  global variables should be avoided in favor of class parameters as
+#  of Puppet 2.6.)
+#
+# Examples
+# --------
+#
+# @example
+#    class { 'puppet_service':
+#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#    }
+#
+# Authors
+# -------
+#
+# Author Name <author@domain.com>
+#
+# Copyright
+# ---------
+#
+# Copyright 2018 Daniel Johnson.
+#
+class puppet_service (
+    $puppet_enabled = true
+  ) {
+  service {'puppet':
+    ensure => running,
+    enable => $puppet_enabled,
+  }
+
+}
